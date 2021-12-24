12a0e4f8280124fcc91a1a14923e7d45d8f8f845
subbarao <s46768@uniper.energy>
2021-12-24 09:29:27 +0000

    comimit

---
+++ Day1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the input number");
+            int n= Convert.ToInt32(Console.ReadLine());
+            for (int i=1;i<=n;i++)
+            {
+                if(i%3==0 && i%5==0)
+                 Console.Write("FizzBuzz,");
+                 else if(i%3==0)
+                    Console.Write("Fizz,");
+                 else if(i%5==0)
+                    Console.Write("Buzz,");
+                 else 
+                    Console.Write(i+",");
+            }
+            Console.ReadLine();
+        }
+    }
+    
+}
